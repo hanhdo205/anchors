f345a25a9a41991a6e2ef2d13b5ad426987d0eb7
hanhdo205 <hanhdo205@gmail.com>
2020-03-20 14:16:19 +0700

    test schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+		$schedule->command('getRank')
+                 ->everyMinute();
+		$schedule->command('getAnchor')
+                 ->everyMinute();
     }
 
     /**
